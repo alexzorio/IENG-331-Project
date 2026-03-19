714b55676e3698ab9fa40d1a4ab59f43d707fb02
Alex Zorio <apz00001@mix.wvu.edu>
2026-03-19 17:40:22 -0400

    Added a key to show all column names, their data types, the tables they belong to, etc. Next is adding rowcounts for all tables.

--- data_quality.sql
+++ data_quality.sql
@@ -1,4 +1,13 @@
--- Telling the script to use the olist file and making a shortcut to reference it
-ATTACH 'olist.duckdb' AS olist;
--- Print all the table names in the schema, remove the semicolon if you want to see all the table names
-SHOW TABLES FROM olist
+-- Telling the script to use the olist file and making a shortcut to reference it *Requires the file to be in the same folder
+-- Remove comment once per running queries
+-- ATTACH 'olist.duckdb' AS olist;
+
+-- Print all the table names in the main schema
+SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'main';
+-- Show all column names, their data type, which table they belong to, and if they accept null values. duckdb has default max number of rows shown
+-- as 40. If you want to change it use ".maxrows xxx"
+SELECT table_name, column_name, data_type, is_nullable
+    FROM information_schema.columns
+    ORDER BY table_name, ordinal_position;
