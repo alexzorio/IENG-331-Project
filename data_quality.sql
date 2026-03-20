a57aa80c2b600c3939a97b11abd1ad00c8e70c68
Alex Zorio <apz00001@mix.wvu.edu>
2026-03-20 14:15:59 -0400

    cleaned up some of the code and comments, moving on to null rates in data quality file

--- data_quality.sql
+++ data_quality.sql
@@ -1,5 +1,4 @@
--- Telling the script to use the olist file and making a shortcut to reference it *Requires the file to be in the same folder
--- Remove comment once per running queries
+-- Telling the script to use the olist file and making a shortcut to reference it !! Requires the database file to be in the same folder !!
 ATTACH 'olist.duckdb' AS olist;
 
 -- Print all the table names in the main schema
@@ -8,13 +7,18 @@ SELECT table_name
     WHERE table_schema = 'main'
         AND table_name NOT IN ('current_notebook_id','has_onboarded','notebook_versions','notebooks')
     ORDER BY table_name;
--- Show all column names, their data type, which table they belong to, and if they accept null values. duckdb has default max number of rows shown
--- as 40. If you want to change it use ".maxrows xxx"
+/*
+Show all column names, their data type, which table they belong to, and if they accept null values.
+*/
 SELECT table_name, column_name, data_type, is_nullable
     FROM information_schema.columns
+    WHERE table_name NOT IN ('current_notebook_id','has_onboarded','notebook_versions','notebooks')
     ORDER BY table_name, ordinal_position;
 
--- Row counts for all tables, making a new table row_counts to store all the new information. Going through each table one by one and linking the counts together in a new table
+/*
+Row counts for all tables, making a new table row_counts to store all the new information.
+Going through each table one by one and linking the counts together in a new table.
+*/
 SELECT
     'category translation' AS table_name,
     'row count' AS info,
@@ -22,50 +26,55 @@ SELECT
 FROM olist.category_translation
 UNION ALL
 SELECT
-    'customers' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'customers',
+    'row count',
+    COUNT(*)
 FROM olist.customers
 UNION ALL
 SELECT
-    'geolocation' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'geolocation',
+    'row count',
+    COUNT(*)
 FROM olist.geolocation
 UNION ALL
 SELECT
-    'order items' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'order items',
+    'row count',
+    COUNT(*)
 FROM olist.order_items
 UNION ALL
 SELECT
-    'order payments' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'order payments',
+    'row count',
+    COUNT(*)
 FROM olist.order_payments
 UNION ALL
 SELECT
-    'order reviews' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'order reviews',
+    'row count',
+    COUNT(*)
 FROM olist.order_reviews
 UNION ALL
 SELECT
-    'orders' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'orders',
+    'row count',
+    COUNT(*)
 FROM olist.orders
 UNION ALL
 SELECT
-    'products' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'products',
+    'row count',
+    COUNT(*)
 FROM olist.products
 UNION ALL
 SELECT
-    'sellers' AS table_name,
-    'row count' AS info,
-    COUNT(*) AS value
+    'sellers',
+    'row count',
+    COUNT(*)
 FROM olist.sellers
 ORDER BY table_name;
+/*
+Finished all the row counts, now moving onto null rates for columns. Directions say key columns,
+I think the key columns when looking at null values are going to be customers, sellers, products, orders, and category translation.
+The other columns are descriptive elements of each order, very important but data that does not make other data or references invalid.
+*/
