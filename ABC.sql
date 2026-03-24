c3629723fa0e43b70a72c9fc92172f97e31f7cc9
Tanzim Rafi <trr0019@mix.wvu.edu>
2026-03-24 17:59:37 -0400

    I went through and added more comments and explanations to the ABC.sql and DeliveryTime.sql files

--- ABC.sql
+++ ABC.sql
@@ -1,10 +1,14 @@
 /*
-This part will sort the data into ABC inventory clasification. This will be based on the parteo principle
+This part will sort the data into ABC inventory clasification. This will be based on the parteo principle to seperarate
+the products into tiers A,B, and C, with the top 20% of revenue getting an A
+the next 15 geting a B
+and the rest getting a C
 
 */
 ATTACH 'olist.duckdb' AS olist;
+
 WITH ProductRevenue AS (
-    -- Step 1: Calculate total revenue per product
+    -- Find the revenue for each product
     SELECT
         product_id,
         SUM(price) as total_revenue
@@ -12,23 +16,23 @@ WITH ProductRevenue AS (
     GROUP BY 1
 ),
 RunningTotals AS (
-    -- Step 2: Use a window function to get cumulative revenue
+    -- Find a cummulative revenue for each product
     SELECT
         product_id,
         total_revenue,
-        SUM(total_revenue) OVER (ORDER BY total_revenue DESC) as cumulative_revenue,
-        SUM(total_revenue) OVER () as grand_total
+        SUM(total_revenue) OVER (ORDER BY total_revenue DESC) as cumulative_revenue, -- Cumulative revenue for each product
+        SUM(total_revenue) OVER () as grand_total -- Grand total revenue for all products
     FROM ProductRevenue
 )
--- Step 3: Assign the Tiers
+-- Use when else statement to seperate the different orders into their respective tiers by revenue percentage
 SELECT
     product_id,
     total_revenue,
-    (cumulative_revenue / grand_total) * 100 as pct_of_total,
+    (cumulative_revenue / grand_total) * 100 as pct_of_total, -- Percentage of total revenue for each product
     CASE
-        WHEN (cumulative_revenue / grand_total) <= 0.80 THEN 'A'
-        WHEN (cumulative_revenue / grand_total) <= 0.95 THEN 'B'
-        ELSE 'C'
+        WHEN (cumulative_revenue / grand_total) <= 0.80 THEN 'A' -- Top 20% of revenue gets an A
+        WHEN (cumulative_revenue / grand_total) <= 0.95 THEN 'B' -- Next 15% gets a B
+        ELSE 'C' -- The rest gets a C
     END as abc_tier
 FROM RunningTotals
-ORDER BY total_revenue DESC;
+ORDER BY total_revenue DESC; -- Order by total revenue descending
